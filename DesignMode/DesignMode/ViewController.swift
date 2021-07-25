7530ef2a37f13ec2931b959d03a6553300af2031
delin <2352915584@qq.com>
2021-07-25 19:33:15 +0800

    fix

---
+++ DesignMode/DesignMode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesignMode
+//
+//  Created by 孟德林 on 2021/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
